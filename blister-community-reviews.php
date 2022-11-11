724ea85e9c5de7c1db8676d7c903409d67fdc7bc
Gunnar Marquardt <gunnargmarquardt@gmail.com>
2022-11-11 14:41:30 -0700

    Initial Commit

---
+++ blister-community-reviews.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Plugin Name: Blister Community Reviews
+ * Description: A plugin to facilitate Blister community created reviews.
+ * Author: Gunnar Marquardt, Jayden Omi, Izak Litte, Jacob Vogel
+ */
