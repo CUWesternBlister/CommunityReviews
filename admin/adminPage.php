2a66c07207eb320886c484bbc29700cc4de64814
Gunnar Marquardt Desktop <gunnargmarquardt@gmail.com>
2022-12-12 15:40:32 -0700

    Create Admin page on activation

---
+++ admin/adminPage.php
@@ -0,0 +1,24 @@
+<?php
+add_action( 'admin_menu', 'bcr_admin_page');
+
+/**
+ * Create the menu page for Blister Community Reviews in wordpress
+ */
+function bcr_admin_page() {
+    add_menu_page(
+        'Community_Reviews',
+        'Community Reviews',
+        'administrator',
+        'bcr_commuinty_reviews',
+        'bcr_admin_page_html',
+        'none'
+    );
+}
+
+/**
+ * HTML for rendering the admin page
+ */
+function bcr_admin_page_html() {
+    
+}
+?>
--- blister-community-reviews.php
+++ blister-community-reviews.php
@@ -12,6 +12,10 @@ register_activation_hook(__FILE__, 'bcr_activation');
 register_deactivation_hook(__FILE__, 'bcr_deactivation');
 add_action( 'plugins_loaded', 'bcr_include');
 
+require_once( plugin_dir_path( __FILE__ ) . '/admin/adminPage.php');
+
+define( 'BCR_PATH', plugin_dir_path( __FILE__ ));
+
 function get_record_from_knowthyself($atts) {
     $atts = shortcode_atts(
 
