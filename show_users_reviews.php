bcb8d0f58a4a4711c5d4202162022ece5638ddfe
Gunnar Marquardt Desktop <gunnargmarquardt@gmail.com>
2023-01-31 23:15:24 -0700

    added query paramater to show only reviews written by a user

---
+++ show_users_reviews.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ * Filter posts so that only those written by the current user show
+ * 
+ */
+function bcr_view_current_user_reviews_only_query( $query ) {
+    if(!is_user_logged_in()) {
+        // Set the query to return nothing
+        $query = new WP_Query(array('post__in' => array(0)));
+    } else {
+        if ( ! function_exists( 'get_current_user_id' ) ) {
+	        return 0;
+	    }
+	    $uid = get_current_user_id();
+        
+        $query->set('author', $uid);
+    }
+}
+
+add_action( 'elementor/query/users_reviews', 'bcr_view_current_user_reviews_only_query');
+?>
