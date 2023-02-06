4261ffe017b338350ea4306d062c7781b0472c5a
Gunnar Marquardt Desktop <gunnargmarquardt@gmail.com>
2023-02-05 23:08:29 -0700

    populate brands dropdown from brands table

--- fluent_form_products_from_database.php
+++ fluent_form_products_from_database.php
@@ -1,28 +1,47 @@
 <?php
-function bcr_set_fluent_form_product_dropdown( $data, $form ) {
+function bcr_set_ski_form_dropdowns( $data, $form ) {
     if ($form->id !=  6) {
-        return $data;
-    }
-
-    if (\FluentForm\Framework\Helpers\ArrayHelper::get($data, 'attributes.name') != '1_1') {
         return $data;
     }
 
     global $wpdb;
 
-    $products_table_name = $wpdb->prefix . "bcr_products";
-
-    $sql = $wpdb->prepare("SELECT productName FROM $products_table_name;");
-
-    $results  = $wpdb->get_results($sql);
+    $input_name = \FluentForm\Framework\Helpers\ArrayHelper::get($data, 'attributes.name');
 
     $new_dropdown = array();
-    foreach ($results as $id => $product_obj) {
-        $dropdown_entry = [
-            "label" => $product_obj->productName,
-            "value" => $product_obj->productName
-        ];
-        array_push($new_dropdown, $dropdown_entry);
+
+    if ($input_name == '1_1') {
+        $products_table_name = $wpdb->prefix . "bcr_products";
+
+        $sql = $wpdb->prepare("SELECT productName FROM $products_table_name;");
+
+        $results  = $wpdb->get_results($sql);
+
+        foreach ($results as $id => $product_obj) {
+            $dropdown_entry = [
+                "label" => $product_obj->productName,
+                "value" => $product_obj->productName
+            ];
+            array_push($new_dropdown, $dropdown_entry);
+        }
+
+    } else if($input_name == '2') {
+        $brands_table_name = $wpdb->prefix . "bcr_brands";
+
+        $sql = $wpdb->prepare("SELECT brandName FROM $brands_table_name;");
+
+        $results  = $wpdb->get_results($sql);
+
+        foreach ($results as $id => $brand_obj) {
+            $dropdown_entry = [
+                "label" => $brand_obj->brandName,
+                "value" => $brand_obj->brandName
+            ];
+            array_push($new_dropdown, $dropdown_entry);
+        }
+
+    } else {
+        return $data;
     }
 
     $data['settings']['advanced_options'] = $new_dropdown;
@@ -30,5 +49,5 @@ function bcr_set_fluent_form_product_dropdown( $data, $form ) {
     return $data;
 }
 
-add_filter('fluentform_rendering_field_data_select', 'bcr_set_fluent_form_product_dropdown', 10, 2);
+add_filter('fluentform_rendering_field_data_select', 'bcr_set_ski_form_dropdowns', 10, 2);
 ?>
