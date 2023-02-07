2183c8a11b78c540f1e33314ba5c9be90097c39f
Gunnar Marquardt Desktop <gunnargmarquardt@gmail.com>
2023-02-06 20:06:20 -0700

    removed unnecessary comments on uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -20,11 +20,6 @@ function bcr_uninstall() {
  */
 function bcr_remove_tables() {
     global $wpdb;
-    
-     //Remove User table
-    $user_table_name = $wpdb->prefix . "bcr_users";
-    $sql = "DROP TABLE IF EXISTS $user_table_name";
-    $wpdb->query($sql);
 
     //Remove Review Answer association table
     $reviews_answers_table_name = $wpdb->prefix . "bcr_reviews_answers";
@@ -36,48 +31,14 @@ function bcr_remove_tables() {
     $sql = "DROP TABLE IF EXISTS $reviews_table_name";
     $wpdb->query($sql);
 
-    /*
-    //Remove Review Form Question assoiciation table
-    $review_forms_questions_table_name = $wpdb->prefix . "bcr_review_forms_questions";
-    $sql = "DROP TABLE IF EXISTS $review_forms_questions_table_name";
-    $wpdb->query($sql);
-    */
-
     //Remove Review Form table
     $review_forms_table_name = $wpdb->prefix . "bcr_review_forms";
     $sql = "DROP TABLE IF EXISTS $review_forms_table_name";
     $wpdb->query($sql);
 
-    /*
-    //Remove Know Thyself Answer association table
-    $know_thyself_answers_table_name = $wpdb->prefix . "bcr_know_thyself_answers";
-    $sql = "DROP TABLE IF EXISTS $know_thyself_answers_table_name";
-    $wpdb->query($sql);
-    */
-
     //Remove Know Thyself table
     $know_thyself_table_name = $wpdb->prefix . "bcr_know_thyself";
     $sql = "DROP TABLE IF EXISTS $know_thyself_table_name";
-    $wpdb->query($sql);
-
-    /*
-    //Remove Know Thyself Form Question association table
-    $know_thyself_forms_questions_table_name = $wpdb->prefix . "bcr_know_thyself_forms_questions";
-    $sql = "DROP TABLE IF EXISTS $know_thyself_forms_questions_table_name";
-    $wpdb->query($sql);
-    */
-
-    /*
-    //Remove Know Thyself Form table
-    $know_thyself_forms_table_name = $wpdb->prefix . "bcr_know_thyself_forms";
-    $sql = "DROP TABLE IF EXISTS $know_thyself_forms_table_name";
-    $wpdb->query($sql);
-    */
-
-
-    //Remove Brands table
-    $brands_table_name = $wpdb->prefix . "bcr_brands";
-    $sql = "DROP TABLE IF EXISTS $brands_table_name";
     $wpdb->query($sql);
 
     //Remove Product table
@@ -85,15 +46,15 @@ function bcr_remove_tables() {
     $sql = "DROP TABLE IF EXISTS $products_table_name";
     $wpdb->query($sql);
 
+     //Remove Brands table
+     $brands_table_name = $wpdb->prefix . "bcr_brands";
+     $sql = "DROP TABLE IF EXISTS $brands_table_name";
+     $wpdb->query($sql);
+
     //Remove Category table
     $categories_table_name = $wpdb->prefix . "bcr_categories";
     $sql = "DROP TABLE IF EXISTS $categories_table_name";
     $wpdb->query($sql);
-
-    //Remove Sport table
-    /*$sports_table_name = $wpdb->prefix . "bcr_sports";
-    $sql = "DROP TABLE IF EXISTS $sports_table_name";
-    $wpdb->query($sql);*/
 
     //Remove Answer table
     $answers_table_name = $wpdb->prefix . "bcr_answers";
@@ -105,7 +66,10 @@ function bcr_remove_tables() {
     $sql = "DROP TABLE IF EXISTS $questions_table_name";
     $wpdb->query($sql);
 
-
+    //Remove User table
+    $user_table_name = $wpdb->prefix . "bcr_users";
+    $sql = "DROP TABLE IF EXISTS $user_table_name";
+    $wpdb->query($sql);
 }
 
 //Execute uninstall
