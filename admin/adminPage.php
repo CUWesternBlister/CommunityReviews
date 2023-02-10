0b4186514b1dbe69c65f3648d215d768e8c31426
Gunnar Marquardt Desktop <gunnargmarquardt@gmail.com>
2023-02-09 18:23:37 -0700

    displays all tables on admin panel

--- admin/adminPage.php
+++ admin/adminPage.php
@@ -24,11 +24,76 @@ function bcr_admin_page() {
  */
 function bcr_admin_page_html() {   
     ?>
-    <div>
+    <div id="bcr_admin">
         <?php
-            echo bcr_display_reviews();
+            global $wpdb;
+
+            $table_names_array = array(
+                $wpdb->prefix . "bcr_users",
+                $wpdb->prefix . "bcr_questions",
+                $wpdb->prefix . "bcr_answers",
+                $wpdb->prefix . "bcr_categories",
+                $wpdb->prefix . "bcr_brands",
+                $wpdb->prefix . "bcr_products",
+                $wpdb->prefix . "bcr_review_forms",
+                $wpdb->prefix . "bcr_reviews",
+                $wpdb->prefix . "bcr_reviews_answers"
+            );
+
+            foreach($table_names_array as $table_name) {
+                bcr_display_table_contents($table_name);
+            }
         ?>
     </div>
+    <?php
+}
+
+/**
+ * Displays the given table as an html table for debugging
+ * 
+ * @param string    table_name
+ * 
+ * @return void
+ */
+function bcr_display_table_contents($table_name) {
+    ?>
+        <div id="<?php echo esc_html($table_name);?>">
+            <h2>
+               <?php echo esc_html($table_name);?> Table 
+            </h2>
+            <?php
+                global $wpdb;
+
+                $esc_table_name = esc_sql($table_name);
+
+                $sql = $wpdb->prepare("SELECT * FROM $esc_table_name");
+
+                $table_data = $wpdb->get_results($sql, ARRAY_A);
+
+                $html = "<table>";
+
+                $html .= "<tr>";
+                foreach($table_data as $row) {
+                    foreach($row as $col_name => $data) {
+                        $html .= "<th>" . esc_html($col_name) . "</th>";
+                    }
+                    break;
+                }
+                $html .= "</tr>";
+
+                foreach($table_data as $row) {
+                    $html .= "<tr>";
+                    foreach($row as $col_name => $data) {
+                        $html .= "<td>" . esc_html($data) . "</td>";
+                    }
+                    $html .= "</tr>";
+                }
+
+                $html .= "</table>";
+                
+                echo $html;
+            ?>
+        </div>
     <?php
 }
 ?>
