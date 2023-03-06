a6bea3f37f54dd138adbc76568082707da5910a5
Jacob Vogel <jacob.vogel@western.edu>
2023-03-06 11:11:00 -0700

    got pages to print out their title from the xml. Think I need to use wordpress importer for parsing.

--- admin/uploadPages.php
+++ admin/uploadPages.php
@@ -6,7 +6,7 @@
  */
 
 // Define the function that will create a new page
-/*function create_test_page() {
+function create_test_page() {
     $post_content = 'Test content';
     $post_title = 'Hello World Testing Page Upload';
     $post_status = 'publish';
@@ -19,6 +19,26 @@
         'post_type' => $post_type
     );
     wp_insert_post( $test_page );
-}*/
+}
+
+/**
+ * parsing xml function
+ *
+ * @return void
+ */
+
+function parse_xml() {
+    $file = fopen(plugin_dir_path( __FILE__ ) . '/xmlTesting.txt', 'w') or die("Unable to open xmlTesting.txt");
+    $filePath = '/Users/jacobvogel/Local Sites/communityreviews-knowthyself/app/public/wp-content/plugins/CommunityReviews/admin/communityreviewprototypes.xml';
+    $xml = simplexml_load_file($filePath) or die("Failed to open XML file.");
+    foreach($xml->channel->item as $item){
+        fwrite($file, $item->title);
+        fwrite($file, "\n");
+        fwrite($file, $item->)
+    }
+    
+}
+
+parse_xml();
 
 ?>
