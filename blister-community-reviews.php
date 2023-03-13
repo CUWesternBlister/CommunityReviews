7639e949e3a31ff057bd298fd1be8708a91da634
IKLr <izak.little@colorado.edu>
2023-03-13 15:48:37 -0600

    sup

--- blister-community-reviews.php
+++ blister-community-reviews.php
@@ -44,19 +44,23 @@ function bcr_include() {
     require_once( plugin_dir_path( __FILE__ ) . 'form_to_custom_post.php');
     require_once( plugin_dir_path( __FILE__ ) . 'show_users_reviews.php');
     require_once( plugin_dir_path( __FILE__ ) . '/admin/adminPage.php');
-<<<<<<< HEAD
-    //require_once( plugin_dir_path( __FILE__ ) .'bcr_review_filter_widget.php');
-=======
     require_once( plugin_dir_path( __FILE__ ) . 'community_reviews_display_functions.php');
->>>>>>> adf10ae760a8c104cac3c6f816224f00f7fb9538
+    //require_once( plugin_dir_path( __FILE__ ) . '/widgets/community-reviews-display.php');
+    // if ( ! class_exists('Community_Reviews_Display')) 
 }
 
 add_action( 'plugins_loaded', 'bcr_include');
 
 function bcr_register_widgets( $widgets_manager ) {
-    require_once( plugin_dir_path( __FILE__ ) . '/widgets/community-reviews-display.php');
-
-    $widgets_manager->register( new \Community_Reviews_Display() );
+    $bool = require_once( plugin_dir_path( __FILE__ ) . '/widgets/community-reviews-display.php');
+    // if ( ! class_exists('Community_Reviews_Display')) 
+    //     die('There is no hope!');
+    if($bool){
+        $temp = new \Community_Reviews_Display();
+        $widgets_manager->register($temp);
+    }
 }
 
 add_action( 'elementor/widgets/register', 'bcr_register_widgets' );
+
+?>
--- widgets/community-reviews-display.php
+++ widgets/community-reviews-display.php
@@ -10,12 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-<<<<<<< HEAD
-class Community_Reviews_Display extends Posts_Base {
-
-	public function get_name() {
-		return 'community_reviews_display';
-=======
 class Community_Reviews_Display extends \Elementor\Widget_Base {
 	public function get_name() {
         return 'community-reviews-display';
@@ -27,82 +21,12 @@ class Community_Reviews_Display extends \Elementor\Widget_Base {
 
 	public function get_categories() {
 		return [ 'community_reviews', 'basic' ];
->>>>>>> adf10ae760a8c104cac3c6f816224f00f7fb9538
 	}
 
     public function get_icon() {
 		return 'eicon-post-list';
 	}
 
-<<<<<<< HEAD
-	protected function register_controls() {
-		parent::register_controls();
-
-		$this->register_query_section_controls();
-		$this->register_pagination_section_controls();
-	}
-
-// ------------------ from posts -------------------
-	public function get_keywords() {
-		return [ 'posts', 'cpt', 'item', 'loop', 'query', 'cards', 'custom post type' ];
-	}
-
-	public function on_import( $element ) {
-		if ( isset( $element['settings']['posts_post_type'] ) && ! get_post_type_object( $element['settings']['posts_post_type'] ) ) {
-			$element['settings']['posts_post_type'] = 'post';
-		}
-
-		return $element;
-	}
-
-	protected function register_skins() {
-		$this->add_skin( new Skins\Skin_Classic( $this ) );
-		$this->add_skin( new Skins\Skin_Cards( $this ) );
-		$this->add_skin( new Skins\Skin_Full_Content( $this ) );
-	}
-
-	public function query_posts() {
-		$query_args = [
-			'posts_per_page' => $this->get_posts_per_page_value(),
-			'paged' => $this->get_current_page(),
-		];
-
-		/** @var Module_Query $elementor_query */
-		$elementor_query = Module_Query::instance();
-		$this->query = $elementor_query->get_query( $this, $this->get_query_name(), $query_args, [] );
-	}
-
-	public function get_query_name() {
-		return $this->get_name();
-	}
-
-	protected function get_posts_per_page_value() {
-		return $this->get_current_skin()->get_instance_value( 'posts_per_page' );
-	}
-
-	protected function register_query_section_controls() {
-		$this->start_controls_section(
-			'section_query',
-			[
-				'label' => esc_html__( 'Query', 'elementor-pro' ),
-				'tab' => Controls_Manager::TAB_CONTENT,
-			]
-		);
-
-		$this->add_group_control(
-			Group_Control_Related::get_type(),
-			[
-				'name' => $this->get_name(),
-				'presets' => [ 'full' ],
-				'exclude' => [
-					'posts_per_page', //use the one from Layout section
-				],
-			]
-		);
-
-		$this->end_controls_section();
-	}
-=======
     protected function _register_controls() {
 
     }
@@ -283,7 +207,6 @@ class Community_Reviews_Display extends \Elementor\Widget_Base {
 		</script>
 		<?php
     }
->>>>>>> adf10ae760a8c104cac3c6f816224f00f7fb9538
 }
 
 
