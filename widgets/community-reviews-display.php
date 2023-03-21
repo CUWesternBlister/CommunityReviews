dc7f20322404a0d962c461328d6edc58f06ac02d
Tristan Riggan <tristan.riggan@colorado.edu>
2023-03-21 13:09:32 -0600

    update

--- widgets/community-reviews-display.php
+++ widgets/community-reviews-display.php
@@ -221,12 +221,15 @@ class Community_Reviews_Display extends \Elementor\Widget_Base {
 					$query = new \WP_Query( $args );
 					
 					if ( $query->have_posts() ) {
-						echo '<ul>';
+
 						while ( $query->have_posts() ) {
+                            echo '<div class="community_review_excerpt">';
 							$query->the_post();
-							echo '<li>' . get_the_title() . get_the_excerpt() . '</li>';
+							echo '<div class="excerpt_title">' . '<a href=' . get_the_permalink() . '>' . get_the_title() . '</a></div>';
+                            echo '<div class="excerpt_content">' . get_the_excerpt() . '</div>';
+                            echo '</div>';
 						}
-						echo '</ul>';
+
 						wp_reset_postdata();
 					}
 				?>
