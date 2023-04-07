abf396037c447c3594d06ed32a48f62907a80cad
Gunnar Marquardt Desktop <gunnargmarquardt@gmail.com>
2023-04-07 11:50:54 -0600

    fixed table_reading_functions.php

--- table_reading_functions.php
+++ table_reading_functions.php
@@ -9,21 +9,10 @@
 function get_question_read_content($q_id){
     global $wpdb;
     $question_table = $wpdb->prefix . "bcr_questions";
-    $q = $wpdb->prepare("SELECT questionDisplayContent, questionType FROM $question_table WHERE questionID = $q_id;");
+    $q = "SELECT questionDisplayContent, questionType FROM $question_table WHERE questionID = $q_id;";
     $q_content = $wpdb->get_row($q);
     return $q_content;
 }
-
-/* ---- this one will be usefull for when product table is filled out.
-function get_product_info($product_name, $file){
-    //$start = "\n\n GET PRODUCT INFORMATION \n";
-    //fwrite($file, $start);
-    $product_table = $wpdb->prefix . "bcr_products";
-    $q = $wpdb->prepare("SELECT * FROM $product_table WHERE productName = '".$product_name."';");
-    $res = $wpdb->get_row($q);
-    return $res;
-}
-*/
 
 //another function that gets brand info and will change the custom post
 function get_brand_info($brand_name, $file){
@@ -31,7 +20,7 @@ function get_brand_info($brand_name, $file){
     //$start = "\n\n GET BRAND INFORMATION \n";
     //fwrite($file, $start);
     $brand_table = $wpdb->prefix . "bcr_brands";
-    $q = $wpdb->prepare("SELECT * FROM $brand_table WHERE brandName = '".$brand_name."';");
+    $q = "SELECT * FROM $brand_table WHERE brandName = '".$brand_name."';";
     $res = $wpdb->get_row($q);
     return $res;
 }
@@ -41,11 +30,11 @@ function get_category_info($form_id, $file){//may just want to return res !!!!!!
     //$start = "\n\n GET CATEGORY INFORMATION \n";
     //fwrite($file, $start);
     $form_table = $wpdb->prefix . "bcr_review_forms";
-    $q1 = $wpdb->prepare("SELECT * FROM $form_table WHERE reviewFormID = $form_id;");
+    $q1 = "SELECT * FROM $form_table WHERE reviewFormID = $form_id;";
     $res1 = $wpdb->get_row($q1);
     $category_id = $res1->categoryID;
     $category_table = $wpdb->prefix . "bcr_categories";
-    $q = $wpdb->prepare("SELECT * FROM $category_table WHERE categoryID = $category_id;");
+    $q = "SELECT * FROM $category_table WHERE categoryID = $category_id;";
     $res = $wpdb->get_row($q);
     return $res;
 }
