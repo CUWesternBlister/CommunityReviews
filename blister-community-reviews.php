fb40d92319d36f462b84a72cb328189389683eb7
Jacob Vogel <jacob.vogel@western.edu>
2023-04-23 14:57:10 -0600

    fixing merge conflicts

--- blister-community-reviews.php
+++ blister-community-reviews.php
@@ -51,11 +51,8 @@ function bcr_include() {
     require_once( plugin_dir_path( __FILE__ ) . 'Profile_Information_Form.php');
     require_once( plugin_dir_path( __FILE__ ) . 'fluent_form_functions.php');
     require_once( plugin_dir_path( __FILE__ ) . 'fluent_forms_scripts.php');
-<<<<<<< HEAD
     require_once(plugin_dir_path( __FILE__ ) . '/admin/adminAddFromCSV.php');
-=======
     require_once( plugin_dir_path( __FILE__ ) . 'review_functions.php');
->>>>>>> a9a1ab039a8e98524a36c1960640f591de2b8b1a
 }
 
 add_action( 'plugins_loaded', 'bcr_include');
