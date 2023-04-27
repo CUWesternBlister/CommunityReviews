bef27a3c67569006746351868bce9ab12a4607ad
reversedpuppy <jaom6502@colorado.edu>
2023-04-27 00:03:49 -0600

    Updated version number izak

--- blister-community-reviews.php
+++ blister-community-reviews.php
@@ -3,7 +3,7 @@
  * Plugin Name: Blister Community Reviews
  * Description: A plugin to facilitate Blister community created reviews.
  * Author: Gunnar Marquardt, Jayden Omi, Izak Litte, Jacob Vogel, Tristan Riggan
- * Version: 1.4.4
+ * Version: 1.4.5
  */
 
 // Exit if accessed directily
